c95f35a23c37fd860670d736004f348d8acac58e
KarinaAlmeida <karinaalmeida523@gmail.com>
2022-10-22 13:49:24 -0300

    finalizado sem bonus

--- script.js
+++ script.js
@@ -5,8 +5,10 @@ function comparador() {
 
 //variáveis globais
 const game = document.querySelector(".game");
-let cardNumber ;
-let click=0; 
+let cardNumber;
+let click=0; //numero de cliques que a pessoa deu
+let match=0; //contabiliza os pares, os acertos das cartas
+let valorGuardado="";
 
 
 alert ("Está pronto para jogar Parrot? Vamos nessa! \n\ By HaveUMetKarina")
@@ -19,10 +21,13 @@ function askCardNumber () {
     }
 
 }
-askCardNumber ();
+
 
 //começo do jogo, criação de cartas e display das mesmas
 function beginGame () {
+    askCardNumber(); 
+
+
 /*gifs aleatorios, pega o número inserido no promot, numcartas, e divide por 2,
 assim, se o usuario inserir 4, pega dois gifs para depois duplicar */
  let cardGifs = parrotGif(cardNumber/2);
@@ -33,14 +38,14 @@ let halfCards = []
 
 for (let i = 0; i < cardNumber/2; i++) {
     halfCards.push( `
-    <div class="cards" onclick="selected (this)">
-    <div class="face back" >
-      <img src="imagens/back.png" alt="">
-    </div>
-    <div class="face front">
-        <img src="${cardGifs[i]}" alt="">
-    </div>
-</div>`)
+    <div class="card" onclick="flip(this)">
+        <div class="face back" >
+            <img src="imagens/back.png" alt="">
+        </div>
+        <div class="face front">
+            <img src="${cardGifs[i]}" alt="">
+        </div>
+    </div>`)
 
 }
 
@@ -52,7 +57,7 @@ let allCards = halfCards.concat(halfCards)
 allCards= allCards.sort(comparador)  //embaralha a array
 
 //aqui, insiro as cartas na div do jogo
-for (let i = 0; i < cardNumber; i++){
+for (let i = 0; i < allCards.length; i++){
     game.innerHTML += allCards[i];
 }
 }
@@ -60,67 +65,65 @@ for (let i = 0; i < cardNumber; i++){
 beginGame(); //fim da função begingame
 
 
-//seleção das cartas
+//virar as cartas
 
-function selected (cards) { //cards ira guardar o this, que é toda a div cards que eu tenho
-click =+1;
+function flip (card) { 
+if (card.classList.contains('turn')===false){ //serve para nao acrescentar valor no valorGuardado, nao comparando com ela mesma
+click ++;
+card.classList.add ('turn');
+card.querySelector(".front").classList.add ('turn');
+card.querySelector(".back").classList.add ('turn');
 
-if (cards.classList.contains ("turn")) { //classe da carta virada, ou seja gif, nada ocorre, é a primiera a ser clicada
 
-}else {
-    flipCard (cards)
+if (valorGuardado !=="") { //so roda se for a segunda carta
+setTimeout(selectedCards, 1500,valorGuardado,card); //chamei a função no timeout
+     //a primeira carta rodou até o if, nao entrou, e card foi armazenado em valorGuardado.
+return valorGuardado=""; //a segunda, vai entrar no if, entao, card ainda vai ser card, nao entra em valorGuardado
+
 }
-flipped = document.querySelectorAll(".cards.turn.onDisplay").length; //a carta virada está na mesa, está dentro do jogo
-
-if (flipped %2==1) { //se a carta virada for de resto 1, é impar, ou seja, só tem ela virada, nada a comparar ainda
-    cards.classList.add("onDisplay") //a adiciono ao jogo
-}else { //preciso compará-las
-    let fCard = document.querySelector (".cards.turn.onDisplay");
-    let sCard= cards 
-
-    if (sameCard(fCard, sCard)){
-        fCard.classList.remove ("onDisplay")
-    }else{
-        
-        fCard.classList.remove("onDisplay");
-        setTimeout(flipCard, 1000, fCard);
-        setTimeout(flipCard, 1000, sCard);
-    }
-
-
+valorGuardado=card;
 }
 }
 
-//virando as cartinhas
-function flipCard (elemento) {
+//aqui coloco esses parametros como strings vazias, para preencher dps.
 
-    elemento.classList.toggle("turn");
+
+
+function selectedCards (firstCard, secondCard) {
+    let carta1= String(firstCard.children[1].children[0].src);
+    let carta2=String(secondCard.children[1].children[0].src);
     
-    let face1 = elemento.querySelector(".face.back");
-    let face2 = elemento.querySelector(".face.front");
 
-    face1.classList.toggle("turn");
-    face2.classList.toggle("turn");
-
-}
-
-//aqui vejo se as cartas sao iguais, ao verificar o immerHTML delas, vejo a des
-function sameCard (fCard,sCard) {
-
-    let cardOne = fCard.innerHTML;
-    let cardTwo = sCard.innerHTML;
-
-    if (cardOne === cardTwo){
-        return true;
-    } else {
-        return false;
+    if (carta1===carta2){
+        match++;
+        gameOver(); //aqui chamo a função para ver se o jogo temrinou
+        
+        } 
+        else {
+            firstCard.classList.remove('turn');
+            firstCard.querySelector(".front").classList.remove ('turn');
+            firstCard.querySelector(".back").classList.remove ('turn');
+            secondCard.classList.remove('turn');
+            secondCard.querySelector(".front").classList.remove ('turn');
+            secondCard.querySelector(".back").classList.remove ('turn');
+            
     }
-
+        
 }
+    
+
+
+
+function gameOver(){
+    if(match === cardNumber/2){
+        alert(`Você ganhou em ${click} jogadas!`)
+    }
+}
+
 
 
 //aqui os nomes de todos os gifs que temos, para inseri-las nas cartas 
-function parrotGif(gifs) {
+ function parrotGif(number) {//number é o parametro que recebo, que, no caso, é o input do usuario
     let cardGifs = [
     'imagens/bobrossparrot.gif', 
     'imagens/explodyparrot.gif', 
@@ -132,11 +135,9 @@ function parrotGif(gifs) {
     ]
     cardGifs = cardGifs.sort (comparador);
 
-    let output = [];
-    for (let i = 0; i < gifs; i++){
-        output.push(cardGifs[i]);
+    let listaGif = []
+    for (let i=0; i <number; i++) {
+        listaGif.push (cardGifs[i])
     }
-
-    return (output);
-
+    return listaGif;
 }
